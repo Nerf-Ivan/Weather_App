15f9b0e125d3410e3d0b8e49ff87a239996a54f9
Nerf-Ivan <ivan.swanepoel.dev@gmail.com>
2025-09-03 21:15:30 +0200

    simple navbar test

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+export default function Navbar() {
+  return (
+  <nav className="bg-black h-15 sticky top-0">
+  <ul className="h-full flex flex-row gap-1.5 justify-center items-center text-white">
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+  </ul>
+</nav>
+  );
+}
